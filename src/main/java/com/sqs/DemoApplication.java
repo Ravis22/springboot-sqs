ea67c606721273e7387ff07385ebca6f8544c5d4
Ravi Kant <ravi.kant@buddy4study.com>
2020-10-28 11:54:49 +0530

    added files

---
+++ src/main/java/com/sqs/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.context.annotation.ConditionalOnAwsCloudEnvironment;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+
+@EnableSqs
+@SpringBootApplication(exclude = {
+        org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextCredentialsAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextResourceLoaderAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.jdbc.AmazonRdsDatabaseAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.metrics.CloudWatchExportAutoConfiguration.class
+})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sqs/config/AWSConfig.java
@@ -0,0 +1,20 @@
+package com.sqs.config;
+
+import org.springframework.cloud.aws.context.annotation.ConditionalOnAwsCloudEnvironment;
+import org.springframework.context.annotation.Import;
+
+@ConditionalOnAwsCloudEnvironment
+@Import({
+        org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextCredentialsAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextResourceLoaderAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.jdbc.AmazonRdsDatabaseAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.metrics.CloudWatchExportAutoConfiguration.class
+})
+public class AWSConfig {
+}
